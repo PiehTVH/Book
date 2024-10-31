84c405ce8f2bfe0f9241a5afa558091ff7e99353
hiep <hiep.tran@aivision.vn>
2024-11-01 02:07:50 +0700

    AuthService

--- server/src/v1/routes/user/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { asyncHandler } = require("../../auth/checkAuth");
-const userController = require("../../controllers/user.controller");
-
-router.post("/getInfo", asyncHandler(userController.getUserInfo));
-router.post("/updateInfo", asyncHandler(userController.updateProfile));
-
-module.exports = router;
